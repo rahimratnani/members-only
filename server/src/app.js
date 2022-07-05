65b131be38115148033689422d41d068e5c33f4f
Rahim Ratnani <rahimratnani4@gmail.com>
2022-07-05 17:01:51 +0530

    Add jwt token generation feature

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 import errorHandler from './utils/error-handler.js';
 import helmet from 'helmet';
 import compression from 'compression';
+import cors from 'cors';
 
 import userRouter from './resources/user/user.router.js';
 import messageRouter from './resources/message/message.router.js';
@@ -16,6 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 // eslint-disable-next-line
 const __dirname = path.dirname(__filename);
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,7 +34,6 @@ app.get('/', (req, res) => {
 });
 
 // Handle 404
-// eslint-disable-next-line
 app.use((req, res, next) => {
   res.status(404);
   throw new Error('Not found');
--- server/src/utils/auth.js
+++ server/src/utils/auth.js
@@ -1,7 +1,55 @@
+import 'dotenv/config';
+import User from './../resources/user/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const newToken = (user) => {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+};
+
+const verifyToken = (token) => {
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    return payload;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
   if (!req.body.name || !req.body.email || !req.body.password) {
     return res.status(400).json({ message: 'Email ,name & pass are required' });
   }
 
-  return res.status(200).json({ message: 'Welcome', ...req.body });
+  try {
+    // check if user already exist
+    // Validate if user exist in the database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res
+        .status(409)
+        .json({ message: 'User already exists. Please login.' });
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = newToken(user);
+
+    console.log(token);
+    return res.status(201).json({ token, id: user.id });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
 };
