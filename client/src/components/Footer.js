7b051f45ebd96ed399e361d8f10e867ee4dddc3c
Rahim Ratnani <rahimratnani4@gmail.com>
2022-07-29 16:25:43 +0530

    Complete footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,21 @@
+import HeartIcon from './../assets/icons/HeartIcon.js';
+import GithubIcon from './../assets/icons/GithubIcon.js';
+
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center p-2 bg-gray-100 absolute left-0 bottom-0 w-full text-center">
-      <h2 className="text-lg font-medium">Footer</h2>
+    <div className="flex space-x-2 justify-center items-center py-8 bg-white font-medium absolute left-0 bottom-0 w-full text-center text-gray-700 text-sm sm:text-base border-t">
+      <p>Made with </p>
+      <span>
+        <HeartIcon className="text-red-600" />
+      </span>
+      <p>by Rahim Ratnani.</p>
+      <a
+        href="##"
+        className="flex justify-center items-center space-x-2 hover:underline"
+      >
+        <span>GitHub</span>
+        <GithubIcon className="h-6 w-6 text-gray-800" />
+      </a>
     </div>
   );
 }
